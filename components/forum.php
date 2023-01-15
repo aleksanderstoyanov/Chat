51fb45166fd16f139d117f38703a5d08db09ecd1
aleksanderstoyanov <blackeder3@gmail.com>
2023-01-15 22:53:49 +0200

    chore(forum): introduce data fetching implementation

--- components/forum.php
+++ components/forum.php
@@ -1,14 +1,17 @@
+<?php include("./database/services/forumService.php")?>
+<?php 
+    $forums = ForumService::readData();
+?>
 <div class="forum-container">
     <h3 class="forum-title">Recent Topics</h3>
     <ul class="forum-items">
+       <?php foreach ($forums as $forum) :?>
         <li class="forum-item">
-           <a href="#"><span class="fa fa-regular fa-comments"></span>Sports</a>
+        <a href="#">
+            <span class="fa fa-regular fa-comments"></span>
+            <?php echo $forum["Name"]?>
+        </a>
         </li>
-        <li class="forum-item">
-            <a href="#"><span class="fa fa-regular fa-comments"></span>Books</a>
-        </li>
-        <li class="forum-item">
-            <a href="#"><span class="fa fa-regular fa-comments"></span>Fun</a>
-        </li>
+        <?php endforeach?>
     </ul>
 </div> 

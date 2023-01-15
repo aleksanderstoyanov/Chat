4c30ad127a5fa68b2423c77c4c994594d9c3eee9
aleksanderstoyanov <blackeder3@gmail.com>
2023-01-15 17:41:37 +0200

    feat(forum): add forum component

---
+++ components/forum.php
@@ -0,0 +1,14 @@
+<div class="forum-container">
+    <h3 class="forum-title">Recent Topics</h3>
+    <ul class="forum-items">
+        <li class="forum-item">
+           <a href="#"><span class="fa fa-regular fa-comments"></span>Sports</a>
+        </li>
+        <li class="forum-item">
+            <a href="#"><span class="fa fa-regular fa-comments"></span>Books</a>
+        </li>
+        <li class="forum-item">
+            <a href="#"><span class="fa fa-regular fa-comments"></span>Fun</a>
+        </li>
+    </ul>
+</div> 
--- index.php
+++ index.php
@@ -10,11 +10,13 @@
     <link rel="stylesheet" href="styles/chat.css">
     <link rel="stylesheet" href="styles/input.css">
     <link rel="stylesheet" href="styles/navigation.css">
+    <link rel="stylesheet" href="styles/forum.css">
     <script src="https://kit.fontawesome.com/1e00da35a2.js" crossorigin="anonymous"></script>    
 </head>
 <body>
     <?php include("./components/header.php")?>
     <main>
+        <?php include("./components/forum.php")?>
         <?php include("./components/chat.php")?>
     </main>
 </body>
